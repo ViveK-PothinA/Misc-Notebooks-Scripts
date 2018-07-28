236227748128be975a7c331a22269e07e935da92
Vivek Pothina <kevivthapion@gmail.com>
2018-07-28 18:56:24 +0530

    Added Merge Sort too

--- SortingInJava.java
+++ SortingInJava.java
@@ -56,7 +56,7 @@ public class SortingInJava{
         return arr;
     }
 
-    //
+    // 
     public int[] insertionSort(int arr[], boolean reverse){
         
         for(int i=0; i<arr.length; i++){
@@ -101,6 +101,59 @@ public class SortingInJava{
         }
     }
 
+    public void merge(int arr[], int l, int m, int h){
+        int leftNum = m-l+1;
+        int rightNum = h-m;
+
+        int[] left = new int[leftNum];
+        int[] right = new int[rightNum];
+
+        for(int i=0; i<leftNum; i++){
+            left[i] = arr[l+i];
+        }
+        for(int j=0; j<rightNum; j++){
+            right[j] = arr[m+1+j];
+        }
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while(i<leftNum && j<rightNum){
+            if(left[i]<=right[j]){
+                arr[k] = left[i];
+                i = i+1;
+                k = k+1;
+            }
+            else{
+                arr[k] = right[j];
+                j = j+1;
+                k = k+1;
+            }
+        }
+        while(i<leftNum){
+            arr[k] = left[i];
+            i = i+1;
+            k = k+1;
+        }
+        while(j<rightNum){
+            arr[k] = right[j];
+            j = j+1;
+            k = k+1;
+        }
+        
+    }
+
+    public void mergeSort(int arr[], int l, int h){
+        
+
+        if(l<h){
+            int m = (l+h)/2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, h);
+            merge(arr, l, m, h);
+        }
+    }    
+
     public static void main(String args[]){
         
         System.out.println(args.length);
@@ -109,19 +162,22 @@ public class SortingInJava{
         int[] array = {53,21,63,32,54,64,62,33,72,66};
 
         // Inversion Count
-        // System.out.println("InversionCount: "+ new Untitled().inversionCount(array));
+        // System.out.println("InversionCount: "+ new SortingInJava().inversionCount(array));
         
         // Selection sort
-        // array = new Untitled().selectionSort(array, true);
+        // array = new SortingInJava().selectionSort(array, true);
 
         // Bubble sort
-        // array = new Untitled().bubbleSort(array, true);
+        // array = new SortingInJava().bubbleSort(array, true);
 
         // Insertion sort
-        // array = new Untitled().insertionSort(array, false);
+        // array = new SortingInJava().insertionSort(array, false);
 
         // Quick sort
-        new Untitled().quickSort(array, 0, array.length-1);
+        // new SortingInJava().quickSort(array, 0, array.length-1);
+
+        // Merge sort
+        new SortingInJava().mergeSort(array, 0, array.length-1);
         
         // For printing .....
         for(int i: array){
